0a4ae884039c419a0d58c81161e44dbaa5e4b315
amstell27 <sergei_i27@mail.ru>
2022-05-05 13:40:38 +0700

    include allure

---
+++ src/test/java/tests/BookStoreTest.java
@@ -0,0 +1,102 @@
+package tests;
+
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.*;
+
+public class BookStoreTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        RestAssured.baseURI = "https://demoqa.com";
+    }
+
+    @Test
+    void getBooksTest() {
+        get("/BookStore/v1/Books")
+                .then()
+                .body("books", hasSize(greaterThan(0)));
+
+    }
+
+    @Test
+    void getBooksWithAllLogsTest() {
+        given()
+                .log().all()
+                .when()
+                .get("/BookStore/v1/Books")
+                .then()
+//                .log().all()
+                .body("books", hasSize(greaterThan(0)));
+
+    }
+
+    @Test
+    void getBooksWithSomeLogsTest() {
+        given()
+                .log().uri()
+                .log().body()
+                .when()
+                .get("/BookStore/v1/Books")
+                .then()
+//                .log().all()
+                .log().status()
+                .log().body()
+                .body("books", hasSize(greaterThan(0)));
+
+    }
+
+    @Test
+    void generateTokenTest() {
+
+        String data = "{\"userName\": \"alex\"," +
+                "\"password\": \"asdsad#frew_DFS2\"}";
+        given()
+                .contentType(JSON)
+                .body(data)
+                .log().uri()
+                .log().body()
+                .when()
+                .post("/Account/v1/GenerateToken")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("status", is("Success"))
+                .body("result", is("User authorized successfully."))
+                .body("token.size()", (greaterThan(10)));
+
+    }
+
+    @Test
+    void generateTokenTesWithAllureListenerTest() {
+
+        String data = "{\"userName\": \"alex\"," +
+                "\"password\": \"asdsad#frew_DFS2\"}";
+//        RestAssured.filters(new AllureRestAssured())
+        given()
+                .filter(new AllureRestAssured())
+                .contentType(JSON)
+                .body(data)
+                .log().uri()
+                .log().body()
+                .when()
+                .post("/Account/v1/GenerateToken")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("status", is("Success"))
+                .body("result", is("User authorized successfully."))
+                .body("token.size()", (greaterThan(10)));
+
+    }
+}
